ede322d0f548086877fda7fb054163d38e9f2235
Abdelhak El Gadyry <ael-gady@e3r7p17.1337.ma>
2025-11-12 07:46:00 +0100

    Update: Get the big picture of IRC and prepare repo.

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+/*
+    => Read RFC 1459 (only sections about commands:
+            NICK, USER, JOIN, PRIVMSG, PART, QUIT, etc.)
+
+    => Create a minimal Server class that binds and listens on a port.
+*/
