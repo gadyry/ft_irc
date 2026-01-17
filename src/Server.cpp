9f2c85fe72113cdab42e58ea22759655258b1cfd
Abdelhak El Gadyry <87965749+gadyry@users.noreply.github.com>
2026-01-17 09:57:42 +0100

    Update: add explain of struct sockaddr_in

--- src/Server.cpp
+++ src/Server.cpp
@@ -5,11 +5,16 @@ Server::Server() {}
 
 Server::Server(u_short port, std::string password) : port(port), password(password), serv_fd(-1)
 {
-    this->serv_fd = socket(IPV4, TCP_SOCK, 0);
+    sockaddr_in addr_in;
+
+    this->serv_fd = socket(PF_INET, SOCK_STREAM, 0);
     if (serv_fd < 0)
         throw std::runtime_error("socket() failed");
 
     
+
+    std::cout << "Server connected\n";
+    std::cout << "Waiting for connection" << std::endl;
 }
 
 Server::~Server()
@@ -17,4 +22,3 @@ Server::~Server()
    close(serv_fd);
    // manage the resources!
 }
-
