6a91875179a645478bb0d73c77baff5b904e8509
gadyry <87965749+gadyry@users.noreply.github.com>
2026-01-29 12:16:40 +0100

    Update: manage replies msg and PASS partgit  add .

---
+++ commandes/handleCmd.cpp
@@ -0,0 +1,16 @@
+# include "Server.cpp"
+# include "Client.cpp"
+
+Server::void    _handleCmd(Client* client, std::vector<std::string>& tokens)
+{
+    std::string& cmd = tokens[0];
+
+    if (client->getReg_stat())
+    {
+        if (cmd == "JOIN")
+            // _handleJoin(...)
+        else if (cmd == "TOPIC")
+            // _handleTopic(...)
+        // ...
+    }
+}
