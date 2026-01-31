cb65c691f96259397093f600ffc7d845a8022d38
Abdelhak El Gadyry <87965749+gadyry@users.noreply.github.com>
2026-01-31 15:34:44 +0000

    ADD: structure of nick cmd

--- commandes/NICK.cpp
+++ commandes/NICK.cpp
@@ -4,11 +4,23 @@
 
 void    Server::_cmdNick(Client* client, std::vector<std::string>& tokens)
 {
+    /*
+        1. Missing nickname? → error
+        2. Invalid characters? → error
+        3. Already in use? → error
+        4. OK? → update client.nick
+        5. Check if client now fully registered
+    */
     if (tokens.empty()) return;
 
+    if (client->checkAuthComplete())
+    {
+        sendError(client, ERR_ALREADYREGISTRED(client->getNickname()));
+        return;
+    }
     if (tokens.size() < 2)
     {
-        sendError(client, "CHEE 9LWA => :IRCServer 461 NICK :Not enough parameters");
+        sendError(client, "Cheee 9lwa tanrjee3 liha !!");
         return;
     }
     if (tokens.size() > 2)
@@ -16,8 +28,5 @@ void    Server::_cmdNick(Client* client, std::vector<std::string>& tokens)
         sendError(client, ERR_ERRONEUSNICKNAME(client->getNickname()));
         return;
     }
-    if (false)
-    {
-        // TODO : Handle somthing!!!
-    }
+
 }
