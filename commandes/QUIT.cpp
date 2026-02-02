52b1f76478aba539241cd6d326562524a2c87b20
gadyry <87965749+gadyry@users.noreply.github.com>
2026-02-02 17:30:05 +0100

    Update: Auth almost finished

---
+++ commandes/QUIT.cpp
@@ -0,0 +1,15 @@
+# include "../includes/Server.hpp"
+# include "../includes/Client.hpp"
+# include "../includes/IrcReplies.hpp"
+
+void    Server::_handleQuit(Client* client)
+{
+    std::cout << "Leehwak 69!!!!!!!" << client->getNickname() << std::endl;
+    /****
+     * 
+     * 
+     * 
+     * 
+     * 
+     ****/
+}
