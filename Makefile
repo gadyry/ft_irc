13106a03b3aec71ed6ec95ed4f27719e6bfb738a
Abdelhak El Gadyry <87965749+gadyry@users.noreply.github.com>
2026-02-06 13:18:08 +0000

    Update: connect MovieBot to server and edit Makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 NAME = ircserv
+BONUS_NAME = MoviesBot
 CPP = c++
 CPPFLAGS = -Wall -Wextra -Werror -std=c++98
 
@@ -6,22 +7,30 @@ SRC = src/main.cpp src/Server.cpp src/Client.cpp commandes/PASS.cpp commandes/ha
 		commandes/NICK.cpp commandes/USER.cpp commandes/QUIT.cpp commandes/JOIN.cpp src/Channel.cpp \
 		commandes/KICK.cpp commandes/MODE.cpp commandes/PRIVMSG.cpp commandes/INVITE.cpp commandes/TOPIC.cpp
 
+BONUS_SRC = bonus/main.cpp bonus/MovieBot.cpp
+
 OBJ = $(SRC:.cpp=.o)
+BONUS_OBJ = $(BONUS_SRC:.cpp=.o)
 
 all: $(NAME)
 
+bonus: $(BONUS_NAME)
+
 $(NAME): $(OBJ)
 	$(CPP) $(CPPFLAGS) -o $(NAME) $(OBJ)
+
+$(BONUS_NAME): $(BONUS_OBJ)
+	$(CPP) $(CPPFLAGS) -o $(BONUS_NAME) $(BONUS_OBJ)
 
 %.o: %.cpp
 	$(CPP) $(CPPFLAGS) -c $< -o $@
 
 clean:
-	rm -f $(OBJ)
+	rm -f $(OBJ) $(BONUS_OBJ)
 
 fclean: clean
-	rm -f $(NAME)
+	rm -f $(NAME) $(BONUS_NAME)
 
 re: fclean all
 
-.PHONY: all clean fclean re
+.PHONY: all bonus clean fclean re
--- bonus/MovieBot.cpp
+++ bonus/MovieBot.cpp
@@ -1,9 +1,9 @@
 # include "../includes/MovieBot.hpp"
 
-MovieBot::MovieBot() : socketBot(-1), nick(""), user(""), servPort(0), hostname("")
-                    password(password), buffRecieve("") { }
+MovieBot::MovieBot() : socketBot(-1), nick(""), user(""), servPort(0), hostname(""),
+                    password(""), buffRecieve("") { }
 
-MovieBot::MovieBot(std:string host, u_short port, std::string password) : socketBot(-1), nick(""), user(""), servPort(port),
+MovieBot::MovieBot(std::string host, u_short port, std::string password) : socketBot(-1), nick("MovieBot"), user("MovieBot test test test"), servPort(port),
                     hostname(host), password(password), buffRecieve("") { }
 
 MovieBot::~MovieBot()
@@ -14,25 +14,29 @@ MovieBot::~MovieBot()
 
 // methods 
 
-void     MovieBot::connect()
+void     MovieBot::connectToServer()
 {
     struct sockaddr_in  addr_serv;
     int                 camus = 1;
-    struct pollfd       newPollFd;
 
     this->socketBot = socket(PF_INET, SOCK_STREAM, 0);
     if (socketBot == -1)
         throw std::runtime_error("failed to create socket!");
 
+    setsockopt(socketBot, SOL_SOCKET, SO_REUSEADDR, &camus, sizeof(camus));
+
     addr_serv.sin_family = PF_INET;
     addr_serv.sin_port = htons(this->servPort);
+    
+    if (inet_aton(this->hostname.c_str(), &addr_serv.sin_addr) <= 0)
+        throw std::runtime_error("Invalid hostname");
 
-    if (connect(socketBot, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    if (connect(this->socketBot, (struct sockaddr *)&addr_serv, sizeof(addr_serv)) < 0)
 		throw std::runtime_error("connect() failed");
 }
 
-// void    MovieBot::executeMovieBot()
-// {
-//     this->connect(); // TODO
-
-// }
+void    MovieBot::executeMovieBot()
+{
+    this->connectToServer();
+    // TODO: Implement bot logic
+}
