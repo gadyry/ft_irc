6bbaa838a75a5c1978a730455d1eac3e6678a29f
gadyry <87965749+gadyry@users.noreply.github.com>
2026-02-10 18:41:13 +0000

    Update: add shared file between bot & Serv

--- src/Server.cpp
+++ src/Server.cpp
@@ -10,40 +10,6 @@ void    Server::signalHandler(int sig)
 	Server::g_signalReceived = 1;
 }
 
-void log(LogLevel level, const std::string &msg)
-{
-	switch (level)
-	{
-		case INFO:
-			std::cout << C_GREEN << C_BOLD << "[SERVER] " << C_RESET << C_GREEN << msg << C_RESET << std::endl;
-			break;
-		case WARN:
-			std::cout << C_YELLOW << "[WARN] " << C_RESET << C_YELLOW << msg << C_RESET << std::endl;
-			break;
-		case ERROR:
-			std::cerr << C_RED << C_BOLD << "[ERROR] " << C_RESET << C_RED << msg << C_RESET << std::endl;
-			break;
-		case DEBUG:
-			std::cout << C_BLUE << "[DEBUG] " << C_RESET << C_BLUE << msg << C_RESET << std::endl;
-			break;
-		case CLIENT:
-			std::cout << C_MAGENTA << "[CLIENT] " << C_RESET << C_MAGENTA << msg << C_RESET << std::endl;
-			break;
-		case NEWCLIENT:
-			std::cout << C_CYAN << "[NEW CLIENT] " << C_RESET << C_CYAN << msg << C_RESET << std::endl;
-			break;
-		case DISCONNECT:
-			std::cout << C_MAGENTA << "[DISCONNECT] " << C_RESET << C_MAGENTA << msg << C_RESET << std::endl;
-			break;
-		case BOT:
-			std::cout << C_GREEN << "[BOT] " << C_RESET << msg << std::endl;  // <-- added only
-			break;
-	}
-}
-
-
-
-
 Server::Server()
 {
 	// handleCommand["NICK"] = &Server::_cmdNick;
@@ -183,7 +149,7 @@ void	Server::_handleLine(Client* client, std::string& fullCmd)
 	std::string cmd = tokens[0];
 	if (cmd == "PING")
 	{
-		std::string token;
+		std::string token = "";
 		size_t spacePos = fullCmd.find(' ');
 		if (spacePos != std::string::npos)
 		{
