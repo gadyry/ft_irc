360053ab97a246078fac2bce00d2b7ed8207ef43
gadyry <87965749+gadyry@users.noreply.github.com>
2026-02-13 16:34:15 +0100

    Update: create struct we needed to loading and manage The core bot

---
+++ bonus/dataBasehandle.cpp
@@ -0,0 +1,13 @@
+# include "../includes/MovieBot.hpp"
+# include "../includes/Server.hpp"
+
+void	MovieBot::loadMovies(std::string db_movies)
+{
+	// TODO
+}
+
+void	MovieBot::loadQuotes(std::string db_quotes)
+{
+	// TODO
+}
+
