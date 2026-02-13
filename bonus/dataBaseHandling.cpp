2e47f18529f965f70a101a83ed3441eab35d52a5
gadyry <87965749+gadyry@users.noreply.github.com>
2026-02-13 16:50:18 +0100

    FIX: correct mistake of repo from Makefile

--- bonus/dataBaseHandling.cpp
+++ bonus/dataBaseHandling.cpp
@@ -3,7 +3,8 @@
 
 void	MovieBot::loadMovies(std::string db_movies)
 {
-	// TODO
+	if (db_movies.empty())
+		throw std::runtime_error("failed to open dataBase");
 }
 
 void	MovieBot::loadQuotes(std::string db_quotes)
